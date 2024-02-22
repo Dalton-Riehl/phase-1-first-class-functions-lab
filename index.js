26fa895c4f7315f93fc15b77f249059d4b436950
Dalton Riehl <dtonriehl@gmail.com>
2024-02-21 21:15:22 -0600

    Advanced Functions, 1st Class Functions Lab Complete: Achievement Get!

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (oldArray) {
+    const newArray = [oldArray[0], oldArray[1]];
+    return newArray;
+}
+
+const returnLastTwoDrivers = function (oldArray) {
+    const newArray = [oldArray[oldArray.length - 2], oldArray[oldArray.length - 1]];
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (melancholy) {
+        return melancholy * multiplier;
+    }
+}
+
+function fareDoubler(num1) {
+    return num1 * 2;
+}
+
+function fareTripler(num2) {
+    return num2 * 3;
+}
+
+function selectDifferentDrivers(turkey, giblets) {
+    const oldArray = turkey;
+    return giblets(oldArray);
+}
